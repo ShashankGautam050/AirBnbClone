488564ae3b9c86f5c223ed9e933b8700422b9914
Shashank Gautam <shashank320722@gmail.com>
2026-02-02 11:28:21 +0530

    created explore view and list item view

---
+++ AirBnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,30 @@
+//
+//  ExploreView.swift
+//  AirBnbClone
+//
+//  Created by Shashank Gautam on 02/02/26.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing : 32){
+                    ForEach(1 ... 10,id: \.self){ listing in
+                       ListingItemViews()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                        
+                    }
+                }
+               
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ AirBnbClone/Core/Listings/ListingItemViews.swift
@@ -0,0 +1,60 @@
+//
+//  ListingItemViews.swift
+//  AirBnbClone
+//
+//  Created by Shashank Gautam on 02/02/26.
+//
+
+import SwiftUI
+
+struct ListingItemViews: View {
+    
+    var lists = [
+        "house1","house2","house4","house3"
+    ]
+    var body: some View {
+        VStack(spacing : 12){
+            TabView{
+                ForEach(lists,id: \.self){ image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .tabViewStyle(.page)
+            
+            HStack(alignment : .top){
+                VStack(alignment : .leading){
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    Text("12 miles away")
+                        .foregroundStyle(.secondary)
+                    Text("3 - 10 nov")
+                        .foregroundStyle(.secondary)
+                    HStack(spacing : 4){
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                Spacer()
+                HStack(spacing : 3){
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+        .padding(.horizontal,24)
+       
+        
+        
+       
+    }
+}
+
+#Preview {
+    ListingItemViews()
+}
