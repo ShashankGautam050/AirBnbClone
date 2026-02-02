f7a14649460bf8e3c4ab2c05f289a5aedd309c86
Shashank Gautam <shashank320722@gmail.com>
2026-02-02 10:43:52 +0530

    Initial Commit

---
+++ AirBnbClone/AirBnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbCloneApp.swift
+//  AirBnbClone
+//
+//  Created by Shashank Gautam on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBnbClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirBnbClone
+//
+//  Created by Shashank Gautam on 02/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
